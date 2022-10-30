0b02892987c864cb81772242e65698326e5b3b54
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-10-30 00:44:59 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
+import withPWA from 'next-pwa'
+import runtimeCaching from 'next-pwa/cache.js'
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === 'development'
+  },
 
   sassOptions: {
     includePaths: [path.join(__dirname, "/src/styles")],
@@ -20,4 +30,13 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const buildConfig = _phase => {
+  const plugins = [withPWA]
+  const config = plugins.reduce((acc, next) => next(acc), {
+    ...nextConfig
+  })
+  return config
+}
+
+export default buildConfig
+
