913893915b1788570b669ce1b54fc97e64ef1dae
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-10-30 23:21:45 -0300

    feat: adds lazy component

--- src/pages/register/[slug].js
+++ src/pages/register/[slug].js
@@ -1,8 +1,10 @@
 import { useRouter } from "next/router";
-import Entrepeuner from "./components/Entrepreneur";
-import User from "./components/User";
+import { lazy, Suspense } from "react";
 
 import styles from "./Register.module.scss";
+
+const User = lazy(() => import("./components/User"));
+const Entrepreneur = lazy(() => import("./components/Entrepreneur"));
 
 export default function Register() {
   const router = useRouter();
@@ -10,7 +12,11 @@ export default function Register() {
 
   return (
     <div className={styles.slug}>
-      <div className={styles.form}>{slug === "user" ? <User /> : <Entrepeuner />}</div>
+      <div className={styles.form}>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          {slug === "user" ? <User /> : <Entrepreneur />}
+        </Suspense>
+      </div>
       <div className={styles.background}>
         <img src="/images/dog-playing.svg" />
       </div>
