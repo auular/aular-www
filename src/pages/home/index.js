3ed745713d58db8f99adb88840d692ddceb75fd7
gabrielsouzaexe <gabrielsouza628@gmail.com>
2022-11-12 18:45:06 -0300

    fix: using font properly

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,18 +8,20 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <Header />
-      <div className={styles.home__hero}>
-        <div className={styles.home__hero_content}>
-          <h1>Vai viajar e não sabe com quem deixar seu pet?</h1>
-          <p>
-            Para nós do AuuLar, o bem-estar do seu pet vem sempre em <span>primeiro
-            lugar.</span>
-          </p>
-          <Link href="/register/user">
-            <BlueButton value="Cadastre-se" />
-          </Link>
+      <div className={styles.home__container}>
+        <div className={styles.home__hero}>
+          <div className={styles.home__hero_content}>
+            <h1>Vai viajar e não sabe com quem deixar seu pet?</h1>
+            <p>
+              Para nós do AuuLar, o bem-estar do seu pet vem sempre em <span>primeiro
+              lugar.</span>
+            </p>
+            <Link href="/register/user">
+              <BlueButton value="Cadastre-se" />
+            </Link>
+          </div>
+          <Shadow />
         </div>
-        <Shadow />
       </div>
     </div>
   );
